f49be2364fad0c3f5ea2a31f26d6948045b40dd4
fahadkhan855 <56050364+fahadkhan855@users.noreply.github.com>
2019-10-06 15:27:34 -0500

    Add files via upload

---
+++ src/main/java/com/fahadkhan855/aws/HelloController.java
@@ -0,0 +1,12 @@
+package com.fahadkhan855.aws;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/hello")
+@RestController
+public class HelloController {
+    public String hello(){
+        return"Hello Youtube";
+    }
+}
